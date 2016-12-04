b7708b88335d1f1ee8c1af9459b500b5db79b2d4
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-04 20:28:21 -0200

    redirect

--- app/src/main/java/com/abxtract/resource/UserResource.java
+++ app/src/main/java/com/abxtract/resource/UserResource.java
@@ -1,20 +1,22 @@
 package com.abxtract.resource;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.abxtract.models.AuthToken;
-import com.abxtract.models.User;
 import com.abxtract.repositories.AuthTokenRepository;
 import com.abxtract.services.UserService;
+import com.abxtract.services.google.GoogleConfig;
 import com.abxtract.services.google.GoogleCredentialService;
 import com.abxtract.services.google.GoogleRedirectService;
 import com.abxtract.services.google.GoogleUserDTO;
@@ -38,16 +40,26 @@ public class UserResource {
 	private GoogleUserService googleService;
 
 	@RequestMapping("/auth/login")
-	public ModelAndView login() {
-		return new ModelAndView( "redirect:" + redirect.buildRedirectUrl() );
+	public void login(@RequestParam("redirect_to") String redirectTo, HttpServletResponse response) throws IOException {
+		response.addCookie( new Cookie( "redirect-to", redirectTo ) );
+		response.sendRedirect( redirect.buildRedirectUrl() );
 	}
 
 	@RequestMapping("/auth/callback")
-	public void callback(@Param("code") String code, HttpServletResponse response) throws IOException {
+	public void callback(
+			@Param("code") String code,
+			HttpServletRequest request,
+			HttpServletResponse response
+	) throws IOException {
 		final GoogleUserDTO dto = googleService.retrieveUserData( credentials.retrieveCredential( code ) );
 		final AuthToken token = authTokens.save( AuthToken.builder().user( service.save( dto ) ).build() );
+		final String uri = Stream.of( request.getCookies() )
+				.filter( cookie -> cookie.getName().equals( "redirect-to" ) )
+				.findFirst()
+				.get()
+				.getValue();
 		response.addCookie( new Cookie( "auth-token", token.getId() ) );
-		response.sendRedirect( "http://localhost:8080/moises" );
+		response.sendRedirect( uri );
 	}
 	//
 	//	@RequestMapping("/user")
