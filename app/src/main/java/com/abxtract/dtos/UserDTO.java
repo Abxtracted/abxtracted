33e3b9eb1271f122c824786a40da634a170eceb8
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-04 17:36:56 -0200

    user dto

---
+++ app/src/main/java/com/abxtract/dtos/UserDTO.java
@@ -0,0 +1,12 @@
+package com.abxtract.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+	private String id, email, name, picture;
+}
