d04b64b166192b41c236c83a4e125eafccb85707
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-04 17:46:55 -0200

    typo

--- app/src/main/java/com/abxtract/config/MvcConfig.java
+++ app/src/main/java/com/abxtract/config/MvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MvnConfig extends WebMvcConfigurerAdapter {
+public class MvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping( "/**" );
