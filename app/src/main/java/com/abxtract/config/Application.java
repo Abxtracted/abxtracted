83e7c43e2b0e4a655589bc3119ae230bcd8a9855
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-04 09:06:01 -0200

    config

--- app/src/main/java/com/abxtract/config/Application.java
+++ app/src/main/java/com/abxtract/config/Application.java
@@ -3,11 +3,13 @@ package com.abxtract.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableOAuth2Sso
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.abxtract")
 @EnableJpaRepositories(basePackages = "com.abxtract.repositories")
