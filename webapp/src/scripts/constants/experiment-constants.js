183d3c0da8311d13f7cd87eee00cdc89616de810
Rafael Camargo <contato@rafaelcamargo.com>
2017-03-25 19:12:55 -0300

    Creating experiment constants

---
+++ webapp/src/scripts/constants/experiment-constants.js
@@ -0,0 +1,12 @@
+(function(){
+
+  app.constant('EXPERIMENT', {
+    ERRORS: {
+      HAS_BEEN_TAKEN: 'This experiment name has already been taken. Please, choose a different one.'
+    },
+    MARGIN_OF_ERROR: {
+      INFINITY: 'Not enough data'
+    }
+  });
+
+})();
