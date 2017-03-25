90965d7e82d13dffc193ba983bf5f548dd98f0e4
Rafael Camargo <contato@rafaelcamargo.com>
2017-03-25 19:08:59 -0300

    Creating route service

---
+++ webapp/src/scripts/services/route-service.js
@@ -0,0 +1,33 @@
+(function(){
+
+  app.service('routeService', [
+    '$window',
+    '$state',
+    '$stateParams',
+    function($window, $state, $stateParams){
+
+    var _public = {};
+
+    _public.url = function(url){
+      var location = $window.location;
+      if(url)
+        location.href = url;
+      else
+        return location.href;
+    }
+
+    _public.go = function(state, params){
+      $state.go(state, params);
+    }
+
+    _public.getParams = function(param){
+      if(param)
+        return $stateParams[param];
+      return $stateParams;
+    };
+
+    return _public;
+
+  }]);
+
+}());
