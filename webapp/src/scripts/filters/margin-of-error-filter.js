384f45c425dbaa1d84df9699d704c50b3d0e7c8a
Rafael Camargo <contato@rafaelcamargo.com>
2017-03-25 19:28:33 -0300

    Improving fitler for margin of error

--- webapp/src/scripts/filters/margin-of-error-filter.js
+++ webapp/src/scripts/filters/margin-of-error-filter.js
@@ -7,7 +7,7 @@
       function filter(marginOfError){
         if(typeof marginOfError == 'string'){
           var KEY = marginOfError.toUpperCase();
-          return EXPERIMENT.MARGIN_OF_ERROR[KEY].TEXT;
+          return EXPERIMENT.MARGIN_OF_ERROR[KEY];
         }
         return marginOfError;
       }
