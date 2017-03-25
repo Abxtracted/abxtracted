c7a0dec742e582ee22cdaff85de8224aa44f37d5
Rafael Camargo <contato@rafaelcamargo.com>
2017-03-25 19:08:49 -0300

    Creating text service

---
+++ webapp/src/scripts/services/text-service.js
@@ -0,0 +1,15 @@
+(function(){
+
+  app.service('textService', function(){
+
+    var _public = {};
+
+    _public.toSnakeCase = function(text){
+      return text.replace(/ /g,'_')
+    };
+
+    return _public;
+
+  });
+
+}());
