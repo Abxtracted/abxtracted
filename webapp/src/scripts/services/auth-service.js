8f7b6aaf924c784b02c4ad2b6d3ac9ece00a0999
Rafael Camargo <contato@rafaelcamargo.com>
2017-03-25 19:17:13 -0300

    Improvements on login form and auth service

--- webapp/src/scripts/services/auth-service.js
+++ webapp/src/scripts/services/auth-service.js
@@ -3,10 +3,18 @@
   app.service('authService', [
     'COOKIES',
     '$cookies',
-    '$location',
-    function(COOKIES, $cookies, $location){
+    'routeService',
+    function(COOKIES, $cookies, routeService){
+
+      var AUTH_URL = environment.apiBaseUrl + 'auth/login';
+      var AUTH_REDIRECT_PARAM = 'redirect_to=' + environment.appBaseUrl;
 
       var _public = {};
+
+      _public.login = function(){
+        var url = [AUTH_URL, AUTH_REDIRECT_PARAM].join('?');
+        routeService.url(url);
+      }
 
       _public.isAuthenticated = function(){
         return $cookies.get(COOKIES.AUTH.TOKEN);
@@ -14,7 +22,7 @@
 
       _public.logout = function(){
         $cookies.remove(COOKIES.AUTH.TOKEN);
-        $location.path('/login');
+        routeService.go('login');
       };
 
       return _public;
