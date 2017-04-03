a9e6a35a159dcb3b422a6c3cb1026cf4ee5262ef
Rafael Camargo <contato@rafaelcamargo.com>
2017-04-03 11:15:07 -0300

    Improving experiment service

--- webapp/src/scripts/services/experiment-service.js
+++ webapp/src/scripts/services/experiment-service.js
@@ -13,6 +13,37 @@
         return EXPERIMENT.STATUS[status];
       };
 
+      _public.isFailingExperiment = function(experimentDetails){
+        return experimentDetails.status == 'invalid';
+      };
+
+      _public.isSuccessfulExperiment = function(experimentDetails){
+        return experimentDetails.status == 'valid';
+      };
+
+      _public.isWinnerScenario = function(experimentDetails, scenario){
+        var winner = getWinnerScenario(experimentDetails);
+        if(winner)
+          return winner.id === scenario.id;
+      };
+
+      _public.isLoserScenario = function(experimentDetails, scenario){
+        var winner = getWinnerScenario(experimentDetails);
+        if(winner)
+          return winner.id !== scenario.id;
+      };
+
+      function getWinnerScenario(experimentDetails){
+        if(_public.isSuccessfulExperiment(experimentDetails))
+          return getBetterConversionVersion(experimentDetails.scenarios);
+      }
+
+      function getBetterConversionVersion(scenarios){
+        if(scenarios[0].converted > scenarios[1].converted)
+          return scenarios[0];
+        return scenarios[1];
+      }
+
       return _public;
 
   }]);
