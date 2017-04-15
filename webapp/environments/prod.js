06938d61ef4d1c165bf94792d3b169d076fc572c
Rafael Camargo <contato@rafaelcamargo.com>
2017-04-15 02:03:38 -0300

    Inserting link to documentation on user menu component

--- webapp/environments/prod.js
+++ webapp/environments/prod.js
@@ -1,4 +1,5 @@
 window.environment = {
   apiBaseUrl: 'https://api.abxtracted.com/',
-  appBaseUrl: 'https://app.abxtracted.com/'
+  appBaseUrl: 'https://app.abxtracted.com/',
+  siteBaseUrl: 'https://www.abxtracted.com/'
 };
