e9db54af9432563f54564a10e0cc7aac09695825
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-04-18 19:36:44 -0300

    wtf

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-build=$(date "+%Y%m%d%H%M")
+# build=$(date "+%Y%m%d%H%M")
+# foda-se vou mudar isso na mao memo to nem ai
+build="v1"
 
 mvn clean install -f ./app/pom.xml
 (cd webapp; npm install; gulp build --end prod)
 
 for img in api web; do
-  docker build -f "Dockerfile.$img" -t "grc.io/abxtract-151511/$img:$build" .
-  gcloud docker -- push "grc.io/abxtract-151511/$img:$build"
+  tag="grc.io/abxtract-151511/$img:$build"
+  docker build -f "Dockerfile.$img" -t "$tag" .
+
+  # not working wtf
+  gcloud docker -- push "$tag"
 done
