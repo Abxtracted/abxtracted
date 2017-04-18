5e257a00895d83b10e84819a2fd69bf54af175f7
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-04-18 19:43:03 -0300

    fix

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,12 +3,12 @@
 # foda-se vou mudar isso na mao memo to nem ai
 build="v1"
 
-# mvn clean install -f ./app/pom.xml
-# (cd webapp; npm install; gulp build --end prod)
+mvn clean install -f ./app/pom.xml
+(cd webapp; npm install; gulp build --end prod)
 
 for img in api web; do
   tag="grc.io/abxtract-151511/$img:$build"
-  # docker build -f "Dockerfile.$img" -t "$tag" .
+  docker build -f "Dockerfile.$img" -t "$tag" .
 
   # not working wtf
   gcloud docker -- push "$tag"
