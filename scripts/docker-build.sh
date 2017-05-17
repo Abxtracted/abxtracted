c8ae1a5090b799e61b812fa9fb967f316b898911
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-05-17 20:37:19 -0300

    && instead of ;

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 build=$(date "+%Y%m%d%H%M")
 
 mvn clean install -f ./app/pom.xml
-(cd webapp; npm install; gulp build --end prod)
+(cd webapp && npm install && gulp build --end prod)
 
 for img in api web; do
   tag="gcr.io/abxtract-167923/$img:$build"
