fcc0d4f5b5946e53861ffb64a292aa4c1bb40718
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-05-17 20:42:01 -0300

    name fix, -x

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-set -eo pipefail
+set -xeo pipefail
 build=$(date "+%Y%m%d%H%M")
 
 mvn clean install -f ./app/pom.xml
 (cd webapp && npm install && gulp build --end prod)
 
 for img in api web; do
-  tag="gcr.io/abxtract-167923/$img:$build"
+  tag="gcr.io/abxtracted-167923/$img:$build"
   docker build -f "Dockerfile.$img" -t "$tag" .
 
   gcloud docker -- push "$tag"
